c249a0142ed4aa6b981e79df5db2ba05db6c9121
pranavpatil70 <pvp3400@gmail.com>
2024-01-12 23:45:49 +0530

    half code

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+print("Welcome to password generator.\n I'll help you to generate a strong password for you!")
+
+alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
+           'V', 'W', 'X', 'Y', 'Z']
+
+numbers = ['1','2','3','4','5','6','7','8','9','0']
+
+symbols = ['!','@','#','$','%','&','*']
+
+numbers_i = int(input("How many numbers would you like in your password?"))
+
+alphabets_i  = int(input("How many alphabets would you like in your password?"))
+
+symbols_i = int(input("How many symbols would you like in our password?"))
